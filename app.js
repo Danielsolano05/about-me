768518ef3413d494a9e807b260e8467743b17cbc
daniel solano <Danielsolano05@gmail.com>
2024-10-29 13:51:56 -0700

    update

---
+++ app.js
@@ -0,0 +1,18 @@
+'use strict';
+function yourName () {
+
+  let name = prompt('what is your name?');
+  return name; 
+}
+let userResponse = confirm('Do you want to proceed?');
+if (userResponse) {
+    console.log('User selected Yes.');
+} else {
+    console.log('User selected No.');
+}
+let userChoose =confirm('was I born in 2005')
+if (userChoose){
+  console.Log('user selected yes.')
+}else {
+  console.log('user selected no.');
+}
